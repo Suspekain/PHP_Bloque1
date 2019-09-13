f15e50fd6fd315dde047b9bb51791982debae0a5
Ekain Susperregi <suspekain@gmail.com>
2019-09-13 10:29:44 +0200

    5. ariketa erdizka

---
+++ 4-multiple3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Comunidad</title>
+</head>
+<body>
+  <?php
+  $numero = 9;
+
+	if ($numero%3 == 0) {
+		echo $numero . " es multiplo de 3";
+	} else {
+		echo $numero . " no es multiplo de 3";
+	}
+
+
+   ?>
+</body>
+</html>
---
+++ 5-palindroma.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Comunidad</title>
+</head>
+<body>
+  <?php
+  $palabra = "oso";
+
+	for ($i=0; $i < ; $i++) {
+		// code...
+	}
+	?>
+</body>
+</html>
