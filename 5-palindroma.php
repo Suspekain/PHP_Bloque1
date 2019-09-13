66f0b839d20350e724f34160cb93bf3479ccb8da
Ekain Susperregi <suspekain@gmail.com>
2019-09-13 11:19:32 +0200

    alig

--- 5-palindroma.php
+++ 5-palindroma.php
@@ -5,10 +5,16 @@
 </head>
 <body>
   <?php
-  $palabra = "oso";
+  $palabra = "osfnsdfho";
+	$letras = str_split($palabra);
 
-	for ($i=0; $i < ; $i++) {
-		// code...
+	for ($i=0; $i < sizeof($letras)/2; $i++) {
+		echo sizeof($letras)-i;
+		if ($letras[i] == sizeof($letras)-i) {
+			// code...
+		} else {
+			// code...
+		}
 	}
 	?>
 </body>
